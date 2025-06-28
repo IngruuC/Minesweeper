9f42696ee62669a0db8d2405d8ec3876f54d3cb3
IngruuC <Ingrid.Carstens@alumnos.uai.edu.ar>
2025-06-28 19:52:26 -0300

    Añadido el archivo de validaciones para el formulario de contacto, incluyendo variables para manejar los campos y errores.

---
+++ scripts/validaciones-contacto.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Variables del formulario de contacto
+var formularioContacto = document.getElementById('formulario-contacto');
+var nombreContacto = document.getElementById('nombre-contacto');
+var emailContacto = document.getElementById('email-contacto');
+var mensajeContacto = document.getElementById('mensaje-contacto');
+var errorNombreContacto = document.getElementById('error-nombre-contacto');
+var errorEmailContacto = document.getElementById('error-email-contacto');
+var errorMensajeContacto = document.getElementById('error-mensaje-contacto');
